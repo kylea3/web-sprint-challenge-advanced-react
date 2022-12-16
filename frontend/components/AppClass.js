4850b1033d1a81ff11b9de852170b56931f1ae96
Kyle Andrus <kyleandrus3@gmail.com>
2022-12-16 16:34:38 -0700

    create getXY, reset, getNextIndex, and move helper functions

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -14,33 +14,83 @@ const initialState = {
 }
 
 export default class AppClass extends React.Component {
+  constructor () {
+    super();
+    this.state = initialState;
+  }
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
 
   getXY = () => {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
+    let x = 0;
+    let y = 0;
+    if(this.state.index <= 2) {
+      y = 1
+    } else if(this.state.index <= 5 && this.state.index > 2) {
+      y = 2
+    } else {
+      y = 3
+    }
+
+    if(this.state.index === 0 || this.state.index === 3 || this.state.index === 6){
+      x = 1
+    } else if(this.state.index === 1 || this.state.index === 4 || this.state.index === 7) {
+      x = 2
+    } else {
+      x = 3
+    }
+    return `Coordinates (${x}, ${y})`;
   }
 
   getXYMessage = () => {
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
+    
   }
 
   reset = () => {
     // Use this helper to reset all states to their initial values.
+    this.setState(initialState);
   }
 
   getNextIndex = (direction) => {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
+    let newIndex = {...this.state}
+    if(direction === 'left' && newIndex.index === 0 || newIndex.index === 3 || newIndex.index === 6 ) {
+      return newIndex.index
+    } else if (direction === 'right' && newIndex.index === 2 || newIndex.index === 5 || newIndex.index === 8 ) {
+      return newIndex.index
+    } else if (direction === 'up' && newIndex.index === 0 || newIndex.index === 1 || newIndex.index === 2 ) {
+      return newIndex.index
+    } else if (direction === 'down' && newIndex.index === 6 || newIndex.index === 7 || newIndex.index === 8 ) {
+      return newIndex.index
+    } else if(direction === 'left') {
+      newIndex.index = newIndex.index - 1;
+    } else if (direction === 'right') {
+      newIndex.index = newIndex.index + 1
+    } else if (direction === 'up') {
+      newIndex.index = newIndex.index - 3
+    } else if (direction === 'down') {
+      newIndex.index = newIndex.index + 3
+    }
+
+    return newIndex.index;
   }
 
   move = (evt) => {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
+    evt.preventDefault();
+    this.setState({
+      ...this.state,
+      index: this.getNextIndex(evt.target.id)
+    })
+
   }
 
   onChange = (evt) => {
@@ -62,8 +112,8 @@ export default class AppClass extends React.Component {
         <div id="grid">
           {
             [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-              <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-                {idx === 4 ? 'B' : null}
+              <div key={idx} className={`square${idx === this.state.index ? ' active' : ''}`}>
+                {idx === this.state.index ? 'B' : null}
               </div>
             ))
           }
@@ -72,11 +122,11 @@ export default class AppClass extends React.Component {
           <h3 id="message"></h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button onClick={this.move} id="left">LEFT</button>
+          <button onClick={this.move} id="up">UP</button>
+          <button onClick={this.move} id="right">RIGHT</button>
+          <button onClick={this.move} id="down">DOWN</button>
+          <button onClick={this.reset} id="reset">reset</button>
         </div>
         <form>
           <input id="email" type="email" placeholder="type email"></input>
