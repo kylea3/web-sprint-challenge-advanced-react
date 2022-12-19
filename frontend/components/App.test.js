31c36f5a1e41a7df1989772bbafec979bd7e2e03
Kyle Andrus <kyleandrus3@gmail.com>
2022-12-19 11:51:17 -0700

    fix spacing in AppClass.js

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
 import AppFunctional from './AppFunctional';
 
 
+const email = document.querySelector('#email');
 // Write your tests here
 test('sanity', () => {
   expect(true).toBe(true)
@@ -20,20 +21,21 @@ test('renders the Coordinates header', () => {
   expect(h3).toBeInTheDocument();
 });
 
-test('renders ONE error message if user submits invalid email', async () => {
+test('renders ONE error message if user submits invalid email', () => {
   render(<AppFunctional />);
   userEvent.type(screen.getByPlaceholderText('type email'), 'Kyle');
   userEvent.click(screen.getByTestId(/submit/i));
-  expect(screen.getByDisplayValue(/Ouch: email must be a valid email/i)).toBeInTheDocument();
-});
+  screen.findByText('Ouch: email is required')
+})
 
-test('typing in input matches input value', async () => {
+test('typing in input matches input value', () => {
   render(<AppFunctional />);
-  userEvent.type(screen.getByPlaceholderText('type email'), 'Kyle');
-  expect(screen.getByDisplayValue(/Kyle/i)).toBeInTheDocument();
+  const email = document.querySelector('#email');
+  fireEvent.change(email, { target: { value: 'Kyle' } });
+  screen.findByText('Kyle');
 });
 
-test('clicking submit resets the email input', async () => {
+test('clicking submit resets the email input', () => {
   render(<AppFunctional />);
   userEvent.type(screen.getByPlaceholderText('type email'), 'sorry@miss.jackson');
   userEvent.click(screen.getByTestId(/submit/i));
